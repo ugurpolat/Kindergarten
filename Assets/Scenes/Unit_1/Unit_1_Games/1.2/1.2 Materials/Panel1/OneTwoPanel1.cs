495a121e717a9f1912bf424dda817f72d6794290
ugurpolat <ugurpolat113@gmail.com>
2020-02-19 18:56:44 +0000

    Chapter two is still on process.

---
+++ Assets/Scenes/Unit_1/Unit_1_Games/1.2/1.2 Materials/Panel1/OneTwoPanel1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneTwoPanel1 : MonoBehaviour
+{
+    public AudioSource applause;
+    public GameObject panel2;
+    public AudioSource sad_1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TrueAnswer()
+    {
+        sad_1.Stop();
+        applause.Play();
+
+        panel2.SetActive(true);
+    }
+
+    public void WrongAnswer()
+    {
+        sad_1.Play();
+    }
+}
