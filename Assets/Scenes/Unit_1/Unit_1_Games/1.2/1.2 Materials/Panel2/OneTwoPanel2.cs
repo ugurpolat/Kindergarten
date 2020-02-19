6aa17e1701df156b422e166bdd9058238e4b56f1
ugurpolat <ugurpolat113@gmail.com>
2020-02-19 23:16:37 +0000

    no message

---
+++ Assets/Scenes/Unit_1/Unit_1_Games/1.2/1.2 Materials/Panel2/OneTwoPanel2.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneTwoPanel2 : MonoBehaviour
+{
+
+    public GameObject img_1;
+    public GameObject img_2;
+    
+    public GameObject panel2;
+    public GameObject panel3;
+
+    public Button btn_1;
+    public Button btn_2;
+
+    public AudioSource clap;
+    public AudioSource sad;
+    public AudioSource question;
+
+    int i = 0;
+    int u = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn_1.interactable = false;
+        btn_2.interactable = false;
+        question.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (question.isPlaying == false)
+        {
+            btn_1.interactable = true;
+            btn_2.interactable = true;
+
+        }
+
+
+    }
+
+    public void Step_1()
+    {
+        img_1.SetActive(true);
+        clap.Play();
+        i = 1;
+        if (u == 1)
+        {
+            StartCoroutine(Waiter());
+        }
+    }
+
+    public void Step_2()
+    {
+        img_2.SetActive(true);
+        clap.Play();
+        u = 1;
+        if (i == 1)
+        {
+            StartCoroutine(Waiter());
+        }
+    }
+
+    IEnumerator Waiter()
+    {
+        yield return new WaitForSeconds(1);
+        panel2.SetActive(false);
+        panel3.SetActive(true);
+
+    }
+}
---
+++ Assets/Scenes/Unit_1/Unit_1_Games/1.2/1.2 Materials/Panel3/OneTwoPanel3.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneTwoPanel3 : MonoBehaviour
+{
+    
+    public AudioSource clap;
+
+    public Button btn_1;
+    public Button btn_2;
+    public Button btn_3;
+    public Button btn_4;
+    public GameObject panel3;
+    public GameObject panel4;
+
+    public int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn_1.onClick.AddListener(delegate { Next(); btn_1.interactable = false; });
+        btn_2.onClick.AddListener(delegate { Next(); btn_2.interactable = false; });
+        btn_3.onClick.AddListener(delegate { Next(); btn_3.interactable = false; });
+        btn_4.onClick.AddListener(delegate { Next(); btn_4.interactable = false; });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter == 4)
+        {
+            StartCoroutine(Waiter());
+        }
+    }
+
+    public void Next()
+    {
+        clap.Play();
+        counter++;
+
+    }
+    IEnumerator Waiter()
+    {
+        yield return new WaitForSeconds(1);
+        panel3.SetActive(false);
+        panel4.SetActive(true);
+
+    }
+}
