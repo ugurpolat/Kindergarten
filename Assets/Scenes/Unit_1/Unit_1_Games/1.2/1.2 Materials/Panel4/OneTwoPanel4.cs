a38c2a7162c6473eb8abaee7dc717ada66da42cc
ugurpolat <ugurpolat113@gmail.com>
2020-02-21 14:42:07 +0000

    1.1-1.2 chapters are done.

---
+++ Assets/Scenes/Unit_1/Unit_1_Games/1.2/1.2 Materials/Panel4/OneTwoPanel4.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OneTwoPanel4 : MonoBehaviour
+{
+    public AudioSource applause;
+
+    public GameObject Next_Button;
+
+    public Button btn_1;
+    public Button btn_2;
+
+    bool a = false;
+    bool b = false;
+
+    public void Step_1()
+    {
+        applause.Play();
+        a = true;
+        if (a == true & b == true)
+        {
+            Next_Button.SetActive(true);
+        }
+    }
+    public void Step_2()
+    {
+        applause.Play();
+        b = true;
+        if (a == true &  b == true)
+        {
+            Next_Button.SetActive(true);
+        }
+    }
+
+}
